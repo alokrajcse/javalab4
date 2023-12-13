c71e9c69b27fc29831b69e19efc4e98227f7e61b
Alok Raj <137365623+alokrajcse@users.noreply.github.com>
2023-12-13 20:20:53 +0530

    Create GCDLCMCalculator.java

---
+++ GCDLCMCalculator.java
@@ -0,0 +1,39 @@
+//LAB QUES4
+
+public class GCDLCMCalculator {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Error: Please provide exactly two integer numbers as arguments.");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int gcd = findGCD(num1, num2);
+        int lcm = findLCM(num1, num2);
+
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
+    }
+
+    private static int findGCD(int n1, int n2) {
+        while (n2 != 0) {
+            int temp = n2;
+            n2 = n1 % n2;
+            n1 = temp;
+        }
+        return n1;
+    }
+
+    private static int findLCM(int n1, int n2) {
+        int lcm = (n1 > n2) ? n1 : n2;
+
+        while (true) {
+            if (lcm % n1 == 0 && lcm % n2 == 0) {
+                return lcm;
+            }
+            ++lcm;
+        }
+    }
+}
